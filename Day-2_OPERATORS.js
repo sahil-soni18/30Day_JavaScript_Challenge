58ab9253727f783afd7350aa2618c23698db5a8a
sahil-soni18 <100425303+sahil-soni18@users.noreply.github.com>
2024-07-14 11:30:13 +0530

    Challenge Day 2

---
+++ Day-2_OPERATORS.js
@@ -0,0 +1,126 @@
+// Activity 1: Arithmetic Operations
+
+// Task 1:
+
+let num1 = 10;
+
+let num2 = 5;
+
+let sum = num1 + num2;
+
+console.log(`Sum: ${sum}`);
+
+// In Short:
+
+console.log(`Sum: ${10 + 5}`);
+
+
+// Task 2:
+
+let subtraction = num1 - num2;
+
+console.log(`Subtraction: ${subtraction}`);
+
+// In Short:
+
+console.log(`Subtraction: ${10 - 5}`);
+
+
+// Task 3: 
+
+let multiplication = num1 * num2;
+
+console.log(`Multiplication: ${multiplication}`);
+
+
+// Task 4:
+
+let Division = num1 / num2;
+
+console.log(`Division: ${Division}`);
+
+
+// Task 5:
+
+let modulus = num1 % num2;
+
+console.log(`Modulus: ${modulus}`);
+
+
+// Activity 2: Assignment Operators
+
+// Task 6:
+
+let a = 10;
+let b = 8;
+
+a += b;
+
+console.log(`a after += b: ${a}`);
+
+// In Short:
+
+console.log(`a += b: ${a += b}`);
+
+
+// Task 7:
+
+a -= b;
+
+console.log(`a after -= b: ${a}`);
+
+// In Short:
+
+console.log(`a -= b: ${a -= b}`);
+
+
+// Activity 3: Comparison Operators
+
+// Task 8:
+
+let c = 10;
+let d = 8;
+
+console.log(`c > d: ${c > d}`);
+console.log(`c < d: ${c < d}`);
+
+// Task 9:
+
+console.log(`c <= d: ${c <= d}`);
+console.log(`c >= d: ${c >= d}`);
+
+// Task 10:
+
+let comp1 = 10;
+let comp2 = "10";
+
+console.log(`comp1 == comp2: ${comp1 == comp2}`);
+console.log(`comp1 === comp2: ${comp1 === comp2}`);
+
+
+// Activity 4: Logical Operators
+
+// Task 11:
+
+let e = true;
+let f = false;
+
+console.log(`e && f: ${e && f}`);
+
+// Task 12:
+
+console.log(`e || f: ${e || f}`);
+
+// Task 13:
+
+console.log(`!e: ${!e}`);
+console.log(`!f: ${!f}`);
+
+
+// Activity 5: Ternary Operator
+
+// Task 14:
+
+let numberToCheck = 2;
+
+console.log(`${numberToCheck} is ${numberToCheck >= 0? "Positive number" : "Negative number"}`);
