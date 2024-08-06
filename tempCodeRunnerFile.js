12928a6bfa8178f180500ea98c08c7d828441dfe
sahil-soni18 <100425303+sahil-soni18@users.noreply.github.com>
2024-08-06 20:01:08 +0530

    Challenge Day 24

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,13 @@
+fetch('https://jsonplaceholder.typicode.com/posts')
+//     .then(response => {
+//         if (!response.ok) {
+//             throw new Error('Network response was not ok ' + response.statusText);
+//         }
+//         return response.json();
+//     })
+//     .then(data => {
+//         console.log(data);
+//     })
+//     .catch(error => {
+//         console.error('Fetch error:', error);
+//     });
